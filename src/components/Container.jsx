097fd9d6d98109ee3ce68cd305c1340a33fa6dc4
Haris Khan <harisk2922@gmail.com>
2024-10-22 16:56:59 +0500

    Navigtaion Bar Completed

---
+++ src/components/Container.jsx
@@ -0,0 +1,12 @@
+const Container = () => {
+    return ( 
+    <main>
+        <div className="left">
+            <h1>YOUR FEET DESERVE THE BEST</h1>
+        </div>
+        <div className="right"></div>
+    </main>
+    )
+};
+
+export default Container
