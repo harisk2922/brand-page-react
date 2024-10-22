3e253425a695a95d4196bcf3244b0a370d02e65f
Haris Khan <harisk2922@gmail.com>
2024-10-22 17:38:07 +0500

    Working on Container Section

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,12 +1,26 @@
 const Container = () => {
     return ( 
-    <main>
-        <div className="left">
-            <h1>YOUR FEET DESERVE THE BEST</h1>
-            
-        </div>
-        <div className="right"></div>
-    </main>
+        <main>
+            <div className="left">
+                <h1>YOUR FEET DESERVE THE BEST</h1>
+                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
+                <div className="buttons">
+                    <button>Shop Now</button>
+                    <button>Category</button>
+                </div>
+                <div className="shopping">
+                    <p>Also Available On</p>
+                    <div className="icons">
+                        <img src="/amazon.png" alt="amazon-logo" />
+                        <img src="/flipkart.png" alt="flipkart-logo" />
+                    </div>
+                </div>
+            </div>
+            <div className="right">
+                <img src="/shoe_image.png" alt="shoe-image" />
+            </div>
+        </main>
     )
 };
 
