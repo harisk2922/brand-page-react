2e092998604bfdbe2dc25df05853b2eed9e90f0e
Haris Khan <harisk2922@gmail.com>
2024-10-23 00:07:42 +0500

    Brand Page Completed!

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,13 +1,13 @@
 const Container = () => {
     return ( 
-        <main className="hero container">
+        <main className="hero hero-section">
             <div className="left">
                 <h1>YOUR FEET DESERVE THE BEST</h1>
-                <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
+                <p className="also">YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
                 YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.</p>
                 <div className="buttons">
                     <button>Shop Now</button>
-                    <button>Category</button>
+                    <button className="secondbtn">Category</button>
                 </div>
                 <div className="shopping">
                     <p>Also Available On</p>
