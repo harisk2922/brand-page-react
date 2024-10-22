bf9cdc38061691ecd71aeb7119aa4c207e4e9fab
Haris Khan <harisk2922@gmail.com>
2024-10-22 17:40:59 +0500

    Working on Container Section

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,6 +1,6 @@
 const Container = () => {
     return ( 
-        <main className="container">
+        <main className="hero container">
             <div className="left">
                 <h1>YOUR FEET DESERVE THE BEST</h1>
                 <p>YOUR FEET DESERVE THE BEST AND WE’RE HERE TO HELP YOU WITH OUR SHOES.
